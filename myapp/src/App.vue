44ba8a2c6c1857df4cbaaaab26e5e6611ec8c9be
darkkisblack <70438354+darkkisblack@users.noreply.github.com>
2021-08-08 21:00:52 +0700

    initial commit

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,34 +1,97 @@
 <template>
-  <div id='App'>
-  <h1>{{ firstName }}</h1>
-  <h2>{{ num > 1 ? 'Number: > 1' : 'Numberone' }}</h2>
-  <h3>{{ obj.age }}</h3>
-  </div>
-
+    <div id="app-container">
+        <DxDataGrid
+            :data-source="employees"
+            key-expr="EmployeeID"
+             :allow-column-resizing="true"
+             :column-auto-width="true"
+             :allow-column-reordering="true">
+            <DxColumn data-field="FullName"></DxColumn>
+            <DxColumn data-field="Position"></DxColumn>
+            <DxColumn
+                data-field="BirthDate"
+                data-type="date">
+            </DxColumn>
+            <DxColumn
+                data-field="HireDate"
+                data-type="date">
+            </DxColumn>
+            <DxColumn data-field="City" />
+             <DxColumn
+              data-field="Country"
+              :group-index="0"
+               sort-order="asc">
+        <DxRequiredRule />
+      </DxColumn>
+            <DxGroupPanel :visible="true" />
+            <DxColumn data-field="Address" />
+            <DxColumn data-field="HomePhone" />
+            <DxColumn data-field="PostalCode" />
+             <DxFilterRow :visible="true" />
+             <DxSearchPanel :visible="true" />
+             <DxEditing
+                mode="popup"
+                :allow-updating="true"
+                :allow-adding="true"
+                :allow-deleting="true"/>
+                <DxSelection mode="single"/>
+                 <DxMasterDetail
+                 :enabled="true"
+                  template="employee-info"/>
+               <template #employee-info="{ data: employee }">
+                <div>
+                 <img class="employee-photo" :src="employee.data.Photo">
+                 <p class="employee-notes">{{ employee.data.Notes }}</p>
+                </div>
+                </template>
+        </DxDataGrid>
+    </div>
 </template>
 
 <script>
+import {
+  DxDataGrid,
+  DxColumn,
+  DxFilterRow,
+  DxSearchPanel,
+  DxEditing,
+  DxSelection,
+  DxMasterDetail,
+} from 'devextreme-vue/data-grid';
+import service from './employees.service';
 
 export default {
   name: 'App',
-
-  data: () => ({
-    firstName: 'roman',
-    num: 7,
-    obj: {
-      age: 30,
-    },
-  }),
+  components: {
+    DxDataGrid,
+    DxColumn,
+    DxFilterRow,
+    DxSearchPanel,
+    DxEditing,
+    DxSelection,
+    DxMasterDetail,
+  },
+  // ...
+  data() {
+    return {
+      employees: service.getEmployees(),
+    };
+  },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style>
+#dataGrid {
+    height: 500px;
+}
+.employee-photo {
+    height: 140px;
+    float: left;
+    padding: 0 20px 20px 0;
+}
+
+.employee-notes {
+    text-align: justify;
+    white-space: normal;
 }
 </style>
